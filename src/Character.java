b5be0eb67c48287938f4dbdf7dcb709f2876134d
SogabeTaiyou <241034@std.hi-joho.ac.jp>
2025-06-30 11:30:29 +0900

    中間課題6/30

--- src/Character.java
+++ src/Character.java
@@ -2,7 +2,6 @@ public abstract class Character implements Creature {
     String name;
     int hp;
     public Character(String name, int hp) {}
-    public abstract void attack(Character target);
     public final boolean isAlive(){
         return this.hp>0;
     }
